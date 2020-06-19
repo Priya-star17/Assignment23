c7a67ab4f9487b50b989ea5c4a283c82a579676c
Priya <priyadalai1393@gmail.com>
2020-06-19 20:58:07 +0530

    Checking Employee Present Absent

---
+++ EmployeePA.java
@@ -0,0 +1,13 @@
+
+public class EmployeePA{
+	public static void main(String arg[]){
+		int IS_PRESENT=1;
+		Random random=new Random();
+		empCheck=random.nextInt(2);
+		if (empCheck == 0)
+      System.out.println("The Employee is Absent.");
+    else
+      System.out.println("The Employee is Present.");
+
+	}
+}
